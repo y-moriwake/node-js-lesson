c283384cb0bc1beab551969670bfcb724b4a2707
y-moriwake <moriwake.yutaka@myutaka-engineer.com>
2021-01-17 18:24:59 +0900

    add mysql

--- src/app.js
+++ src/app.js
@@ -1,43 +1,63 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql');
 const app = express();
+
+////#region サーバ設定
+
+////#region Mysql接続設定
+
+// mysql接続設定
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    port: 3306,
+    user: 'user',
+    password: 'password',
+    database: 'sample_database'
+})
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('connected mysql')
+});
+
+////#endregion
 
 // サーバー設定
 const server = app.listen(4000, () => {
     console.log('Node.js is listening to PORT:' + server.address().port);
 });
 
+// expressの設定
 app.disabled('x-powered-by');
-app.use(bodyParser.json());
+app.use(cors()).use(bodyParser.json());
 
-const sampleData = [
-    {
-        id: 0,
-        title: 'sampleData1',
-        description: 'samnpleData1'
-    },
-    {
-        id: 1,
-        title: 'sampleData2',
-        description: 'samnpleData2'
-    },
-];
+////#endregion
 
 // getリクエスト
 app.get('/', (req, res, next) => {
-    res.json(sampleData);
+    const sql = 'select * from todos where `id` = 2';
+    connection.query(sql, (err, results) => {
+        if(err) throw err;
+        res.json(results);
+    });
 });
 
 // getリクエスト(id指定)
 app.get('/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
-    const data = sampleData.find((data) => data.id === id);
-    res.json(data);
+    const sql = 'select * from todos where ?';
+    connection.query(sql, {id: id},(err, results) => {
+        if(err) throw err;
+        res.json(results[0]);
+    });
 });
 
-// Postリクエスト
-app.post('/', (req, res, next) => {
-    const data = req.body;
-    console.log(data);
-    res.json(data);
-});
+// // Postリクエスト
+// app.post('/', (req, res, next) => {
+//     const data = req.body;
+//     console.log(data);
+//     res.json(data);
+// });
